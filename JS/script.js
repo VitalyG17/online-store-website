617d2de2dbddb8e0fdeae7e0ccd8b541db67f149
Vitaly Gulyaev <VitalyG-17@yandex.ru>
2024-02-15 02:59:38 +0300

    Added the ability to add products to the cart and “order” them

--- JS/script.js
+++ JS/script.js
@@ -18,7 +18,7 @@ document.readyState == 'loading'
   : ready();
 
 function ready() {
-  formatingPriceCard()
+  formatingPriceCard();
   let removeCartButtons = document.getElementsByClassName('cart-remove');
   console.log(removeCartButtons);
   for (let i = 0; i < removeCartButtons.length; i++) {
@@ -30,9 +30,17 @@ function ready() {
     let input = quantityInputs[i];
     input.addEventListener('change', ChekingQuantityChanged);
   }
+  let addCart = document.getElementsByClassName('add-cart');
+  for (let i = 0; i < addCart.length; i++) {
+    let button = addCart[i];
+    button.addEventListener('click', addCartClicked);
+  }
+  document
+    .getElementsByClassName('btn-buy')[0]
+    .addEventListener('click', buyButtonClicked);
 }
 
-//Сhecking data, calling price update
+// Сhecking data, calling price update
 function ChekingQuantityChanged(event) {
   let input = event.target;
   if (input.value == NaN || input.value <= 0) input.value = 1;
@@ -43,6 +51,56 @@ function ChekingQuantityChanged(event) {
 function removeCartItem(event) {
   let buttonClicked = event.target;
   buttonClicked.parentElement.remove();
+  updateTotalPrice();
+}
+
+// Add to cart
+function addCartClicked(event) {
+  let button = event.target;
+  let shopProducts = button.parentElement;
+  let title = shopProducts.getElementsByClassName('product-title')[0].innerText;
+  let price = shopProducts.getElementsByClassName('price')[0].innerText;
+  let image = shopProducts.getElementsByClassName('product-img')[0].src;
+  addProductToCart(title, price, image);
+  updateTotalPrice();
+}
+
+function addProductToCart(title, price, image) {
+  let cartShopBox = document.createElement('div');
+  cartShopBox.classList.add('cart-box');
+  let cartItems = document.getElementsByClassName('cart-content')[0];
+  let cartItemsNames = cartItems.getElementsByClassName('cart-production-title');
+  for (let i = 0; i < cartItemsNames.length; i++) {
+    if (cartItemsNames[i].innerText == title) {
+      alert('You have already add this item to cart');
+      return;
+    }
+  }
+  let cartBoxContainer = `<img src="${image}" alt="" class="cart-img" />
+                          <div class="detail-box">
+                            <div class="cart-production-title">${title}</div>
+                            <div class="price">${price}</div>
+                            <input type="number" value="1" class="cart-quantity" />
+                          </div>
+                        <i class="bx bxs-trash-alt cart-remove"></i>`;
+
+  cartShopBox.innerHTML = cartBoxContainer;
+  cartItems.append(cartShopBox);
+  cartShopBox
+    .getElementsByClassName('cart-remove')[0]
+    .addEventListener('click', removeCartItem);
+  cartShopBox
+    .getElementsByClassName('cart-quantity')[0]
+    .addEventListener('change', ChekingQuantityChanged);
+}
+
+// Buy Button
+function buyButtonClicked() {
+  let cartContent = document.querySelector('.cart-content');
+  cartContent.children.length === 0
+    ? alert('Your cart is empty')
+    : alert('Your order has been created');
+  cartContent.innerHTML = '';
   updateTotalPrice();
 }
 
